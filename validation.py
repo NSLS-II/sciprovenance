d45044ac5ec7021dc86e8eda2aef0c161b5d65cf
hsabrina <39924878+hsabrina@users.noreply.github.com>
2018-07-09 16:09:33 -0400

    dash-to-underscore in keys only
    Changed where the dash-to-underscore change in key names occurs. Previously ALL dashes were changed to underscores, including those that may be in value strings. Now only dashes in key names are changed to underscores, allowing users to have dashes in their responses if desired.
    Updated formatting to Black formatting. 
    Added comments before the split_dict for loop.

--- validation.py
+++ validation.py
@@ -23,10 +23,10 @@ db = client["samples"]
 coll = db["test"]
 
 try:
-  db.command("serverStatus")
+    db.command("serverStatus")
 except Exception as e: print(e)
 else:
-  print("You are connected!")
+    print("You are connected!")
 
 # need to download spreadsheet as a csv file using Google APIs
 # part of this code was taken from wescpy.blogspot.com
@@ -36,8 +36,8 @@ SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
 store = file.Storage('storage.json')
 creds = store.get()
 if not creds or creds.invalid:
-  flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-  creds = tools.run_flow(flow, store)
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
 DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
 
 FILENAME = str(input("Please enter filename: "))
@@ -45,59 +45,75 @@ SRC_MIMETYPE = 'application/vnd.google-apps.spreadsheet' # source file type
 DST_MIMETYPE = 'text/csv' # exported file type
 
 # finds file in Google Drive
-files = DRIVE.files().list(
-  q='name="%s" and mimeType="%s"' % (FILENAME, SRC_MIMETYPE),
-  orderBy='modifiedTime desc,name').execute().get('files', [])
+files = (
+    DRIVE.files()
+    .list(
+        q='name="%s" and mimeType="%s"' % (FILENAME, SRC_MIMETYPE),
+        orderBy='modifiedTime desc,name'
+    )
+    .execute()
+    .get('files', [])
+)
 
 if files: # if file was found
-  fn = '%s.csv' % os.path.splitext(files[0]['name'].replace(' ', '_'))[0] # create csv file with underscores
-  print('Exporting "%s" as "%s"... ' % (files[0]['name'], fn), end='')
-  data = DRIVE.files().export(fileId=files[0]['id'], mimeType=DST_MIMETYPE).execute()
-  if data:
-    with open(fn, 'wb') as f:
-      f.write(data)
-    print('DONE') # done exporting as csv file
-    Hone = hone.Hone() # convert csv file to nested json format, result is a list
-    schema = Hone.get_schema(fn)
-    result = Hone.convert(fn)
-    split_dict_list = []
-    for d in result:
-      split_dict = {}
-      for key, value in d.items():
-        if isinstance(value, str) == True and ';' in value:
-          split_dict[key] = value.split(';')
-        elif isinstance(value, dict) == True:
-          split_dict[key] = {}
-          for new_key, new_value in value.items():
-            if isinstance(new_value, str) == True and ';' in new_value:
-              split_dict[key][new_key] = new_value.split(';')
-            elif isinstance(new_value, dict) == True:
-              split_dict[key][new_key] = {}
-              for next_key, next_value in new_value.items():
-                if isinstance(next_value, str) == True and ';' in next_value:
-                  split_dict[key][new_key][next_key] = next_value.split(';')
+    fn = (
+        '%s.csv' % os.path.splitext(files[0]['name'].replace(' ', '_'))[0]
+    ) # create csv file with underscores
+    print('Exporting "%s" as "%s"... ' % (files[0]['name'], fn), end='')
+    data = DRIVE.files().export(fileId=files[0]['id'], mimeType=DST_MIMETYPE).execute()
+    if data:
+        with open(fn, "wb") as f:
+            f.write(data)
+        print("DONE")  # done exporting as csv file
+        Hone = hone.Hone()  # convert csv file to nested json format, result is a list
+        schema = Hone.get_schema(fn)
+        result = Hone.convert(fn)
+        split_dict_list = [] # this is where the final list of dictionaries is inserted
+        # need to split strings that contain multiple values separated with ';' to create arrays
+        # need to rename key names to contain underscores instead of dashes
+        # since there is multiple nestings in the schema, for loops and if statements are also nested
+        for d in result:
+            split_dict = {}
+            for key, value in d.items():
+                key = key.replace("-", "_")
+                if isinstance(value, str) == True and ";" in value:
+                    split_dict[key] = value.split(";")
+                elif isinstance(value, dict) == True:
+                    split_dict[key] = {}
+                    for new_key, new_value in value.items():
+                        new_key = new_key.replace("-", "_")
+                        if isinstance(new_value, str) == True and ";" in new_value:
+                            split_dict[key][new_key] = new_value.split(";")
+                        elif isinstance(new_value, dict) == True:
+                            split_dict[key][new_key] = {}
+                            for next_key, next_value in new_value.items():
+                                next_key = next_key.replace("-", "_")
+                                if (
+                                    isinstance(next_value, str) == True
+                                    and ";" in next_value
+                                ):
+                                    split_dict[key][new_key][
+                                        next_key
+                                    ] = next_value.split(";")
+                                else:
+                                    split_dict[key][new_key][next_key] = next_value
+                        else:
+                            split_dict[key][new_key] = new_value
                 else:
-                  split_dict[key][new_key][next_key] = next_value
-            else:
-              split_dict[key][new_key] = new_value
-        else:
-          split_dict[key] = value
-      split_dict_list.append(split_dict)
-    as_string = json.dumps(result) # convert resulting list into string
-    replace_all_dashes = as_string.replace('-', '_') # replace dashes with underscores
-    final_result = json.loads(replace_all_dashes) # convert string back into list of dictionaries
-    # since result is a list, iterate validation over each item:
-    number = 0
-    for data in final_result:
-      number += 1
-      with open('materials.json', 'r') as file:
-        loaded_schema = file.read()
-        validation_schema = ast.literal_eval(loaded_schema)
-        validate(data, validation_schema)
-        print('Sample %s validation complete!' % number) # prints only if no error occurs on validation step
-        doc_id = coll.insert_one(data).inserted_id # insert validated schema into MongoDB, only occurs if there is no error
-        print('The ID for this document is: ', doc_id)
-  else:
-    print('ERROR (could not download file)')
+                    split_dict[key] = value
+            split_dict_list.append(split_dict)
+        # since result is a list, iterate validation over each item:
+        number = 0
+        for data in split_dict_list:
+            number += 1
+            with open("materials.json", "r") as file:
+                loaded_schema = file.read()
+                validation_schema = ast.literal_eval(loaded_schema)
+                validate(data, validation_schema)
+                print('Sample %s validation complete!' % number) # prints only if no error occurs on validation step
+                doc_id = coll.insert_one(data).inserted_id # insert validated schema into MongoDB
+                print('The ID for this document is: ', doc_id)
+    else:
+        print("ERROR (could not download file)")
 else:
-  print('ERROR: file not found')
+    print("ERROR: file not found")
